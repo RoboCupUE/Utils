76065ca2813b994219aa41be7f51018e2a03c29e
fgonzalezr1998 <fergonzaramos@yahoo.es>
2024-11-07 22:46:03 +0100

    [scripts] install_humble script added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Utils
+
+This repository contains scripts and other useful assets that help with ROS installation, ROS-like robotic/computing platforms
+setup, etc.
+
+Hereafter, a brief description is provided about the content.
+
+## Scripts
+
+Under *scripts* folder contains the following scripts:
+
+* **install_humble.bash:** Install ros-humble-desktop-full. Make sure the computer meets the SO requirements (Debian based and
+Ubuntu 22.04 LTS or equivalent).
---
+++ scripts/install_humble.bash
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install ros-humble-desktop
+sudo apt install ros-dev-tools
+
+echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+source ~/.bashrc
