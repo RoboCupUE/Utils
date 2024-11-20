41bd17f38a35aed306cd857f8c8aeac89361e73e
Daniel <hevcuesta@gmail.com>
2024-11-20 19:28:27 +0100

    added tb4 installation script

---
+++ scripts/install_tb4_sim.sh
@@ -0,0 +1,7 @@
+sudo apt install ros-humble-turtlebot4-simulator ros-humble-irobot-create-nodes
+sudo apt install ros-dev-tools
+
+sudo apt-get update && sudo apt-get install wget
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt-get update && sudo apt-get install ignition-fortress
